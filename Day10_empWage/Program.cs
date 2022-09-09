6624ffe5fee483a10e75e08b590184511033a0a6
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-09-09 18:29:15 +0530

    [Abhi]Added:Ability to save empWage for each company

---
+++ Day10_empWage/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Day10_empWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilder Dmart = new EmpWageBuilder("Dmart", 20, 2, 10);
+            EmpWageBuilder Reliance = new EmpWageBuilder("Reliance", 10, 4, 20);
+            Dmart.ComputeWage();
+            Console.WriteLine(Dmart.ToString());
+            Reliance.ComputeWage();
+            Console.WriteLine(Reliance.ToString());
+        }
+    }
+}
